fc9e5d38fa3628e320b753c0feae9513d60e293c
JUMINAHN <jumin9774@naver.com>
2024-07-29 01:01:34 +0900

    update

--- 0728/python_practice/day4/s1690.py
+++ 0728/python_practice/day4/s1690.py
@@ -20,25 +20,30 @@ for i in range(1, 11):
         dummy_data.append(new_dict)
 
 
-def censorship(censored_user_list) :#blacklist 유무 확인하기 -> 전달받은 company, name 기반
-    for user in censored_user_list:
-        if user['company'] in black_list:
-            print(f'{user["company"]} 소속의 {user["name"]} 은/는 등록할 수 없습니다.')
-            return False
-        else :
-            print('이상 없습니다.')
-            return True
+def censorship(dummy) :#blacklist 유무 확인하기 -> 전달받은 company, name 기반
+    if dummy['company'] in black_list:
+        print(f'{dummy["company"]} 소속의 {dummy["name"]} 은/는 등록할 수 없습니다.')
+        return False
+    else:
+        print('이상 없습니다.')
+        return True
+    
         
 
-def create_user(dummy_data): #사용자 목록을 인자로 넘겨 순회하는 코드
-    #사용자 목록을 순회하면서 각 사용자 정보를 censorshop 인자로 넘겨 balcklist에 포함되어 있는지 확인한다.
+def create_user(dummy_data): 
+    #순회하여 company 이름을 key로, 사용자이름을 value(리스트)로 가지는 dict 생성
+    #순회하면서 각 사용자 정보를 censorship함수에 넘겨서 balcklist에 포함되어있는지 확인
     censored_user_list = {}
-    for dummy in dummy_data:
-        censored_user_list.update({dummy['company'] : list(dummy['name'])}) #company이름을 key로 사용자 이름을 value는 list로 구성한다.
-    censorship(censored_user_list) #blacklist 유무 확인하기
+    for dummy in dummy_data: #dummy안에 한가지 리스트가 있을 것
+        result = censorship(dummy)
+        if result :
+            censored_user_list.setdefault(dummy['company'], [dummy['name']])
+    return censored_user_list
+
 
 result = create_user(dummy_data)
 print(result)
+
 
 
 
