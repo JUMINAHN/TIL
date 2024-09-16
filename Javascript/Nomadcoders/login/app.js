53ec6b19b564ce00ab5fd404cea05f6bda1ac3ed
JUMINAHN <jumin9774@naver.com>
2024-09-16 21:30:32 +0900

    update

--- Javascript/Nomadcoders/login/app.js
+++ Javascript/Nomadcoders/login/app.js
@@ -3,48 +3,37 @@ const loginInput = document.querySelector("#login-form input")
 const greeting = document.querySelector("#greeting")
 const linkData = document.querySelector("a") 
 
-const HIDDEN_CLASSNAME = "hidden";
-//반복적으로 발생하는 내용 변수 설정하기 --> userName
-const USERNAME_KEY = "username"; //오타로 발생할 수 있는 추가 이슈 사전 대응
+//prevent
 
-//똑같은 작업 반복
-function paintGreetings(userName) {
-  greeting.innerText = `Hello ${userName}` //저장된 data를 반영할 것이기 떄문에
-  greeting.classList.remove(HIDDEN_CLASSNAME); //form 삭제
-}
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"; 
+
+function paintGreetings() {
+  const userName = localStorage.getItem(USERNAME_KEY);
+  //local에 저장되면 호출하는 순간에 이미 userdata는 이미 저장되어 있을 것
+  //local을 두번 열어보는 것
+  greeting.innerText = `Hello ${userName}` 
+  greeting.classList.remove(HIDDEN_CLASSNAME); }
 
 function onLoginSubmit(event) {
   event.preventDefault(); 
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const userName = loginInput.value;
-  //greeting.innerText = `Hello ${userName}`
+  //const userName = loginInput.value;
 
-  //greeting.classList.remove(HIDDEN_CLASSNAME);
-  paintGreetings(userName);
-  localStorage.setItem(USERNAME_KEY, userName); //키와 값 적기
+  paintGreetings();
+  localStorage.setItem(USERNAME_KEY, userName); 
   console.log(userName);
 }
 
-//localStorage에 값이 없다면 null이 나옴
-//값이 담기게 될 것
 const savedUserName = localStorage.getItem(USERNAME_KEY); 
-console.log(savedUserName);
+//localstorange에 유저 정보가 존재하는 것을 알고 있다.
 
-//만약 userName이 없다면 form을 보여줄 것이고,
-//userName이 있다면 화면에 나의 이름을 보여줄 것
-//즉 local 정보에 userName이 없을떄 
 if (savedUserName === null) {
-  //form, greeting 자체를 모두 숨기고 시작한다.
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
-  //없으면 자체적으로 반영을 시킨다.
 } else {
-  //그게 아니라면 이름을 보여줄 것
-  //show greetings
   const userName = loginInput.value;
-  paintGreetings(savedUserName);
-  //greeting.innerText = `Hello ${savedUserName}` //저장된 data를 반영할 것이기 떄문에
-  //greeting.classList.remove(HIDDEN_CLASSNAME); //form 삭제
+  paintGreetings();
 }
 
 
