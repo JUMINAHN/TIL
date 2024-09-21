cac215802cc5ebb709863734cdb0fe0e037d70b9
JUMINAHN <jumin9774@naver.com>
2024-09-21 16:58:26 +0900

    update files

--- Django/Assginment/django_ws_2_a/todo_list_project/settings.py
+++ Django/Assginment/django_ws_2_a/todo_list_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'todo_list_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['todo_list_project/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
