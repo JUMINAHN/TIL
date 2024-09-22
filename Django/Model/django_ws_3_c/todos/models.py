be19f25d47a14d156b198aa6f71cb7ca6e4f07ff
JUMINAHN <jumin9774@naver.com>
2024-09-22 12:17:36 +0900

    update files -> models

---
+++ Django/Model/django_ws_3_c/todos/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    work = models.CharField(max_length=100)
+    content = models.TextField()
+    is_completed = models.BooleanField()
