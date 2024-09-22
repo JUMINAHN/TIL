9785c60d0f098727db3453058010ac1c6d1f0053
JUMINAHN <jumin9774@naver.com>
2024-09-22 23:38:56 +0900

    modified data

--- Algorithm/Study/Week9/BOJ_1260_onlyDFS.py
+++ Algorithm/Study/Week9/BOJ_1260_onlyDFS.py
@@ -4,7 +4,7 @@
 #각 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. == 양방향이다.
 
 
-#예제 입력1 케이스는 맞게 나온다.
+#예제 입력1, 입력3 케이스는 맞게 나온다.
 def DFS(startV, endV): #시작노드, 끝노드
     stack = []
     visited = [False] * (endV+1) #끝노드 +1 정점 노드 개수만큼
@@ -27,7 +27,6 @@ def DFS(startV, endV): #시작노드, 끝노드
             stack.pop()
     return result
 
-
 N, M, V = map(int, input().split()) #정점의 개수 N개, #간선의 개수 M개, 탐색 시작할 정점 번호
 #간선의 개수만큼 input을 받는다.
 #인접 리스트 생성 -> 정점의 개수 + 1개만큼
@@ -37,5 +36,14 @@ for _ in range(M):
     adjList[v1].append(v2)
     adjList[v2].append(v1) #각 idx에 맞는 node 입력
 #print(adjList) #[[], [2, 3, 4], [1, 4], [1, 4], [1, 2, 3]]
+#2차원 자체에 솔트를 하면? == 값이 이상해진다.
+# adjList.sort()
+# print(adjList)
+
+#그렇다면 배열로 순회를 해서 각 노드를 정렬한다면? == 원하는 의도로 나올 것 같긴함
+for adj in adjList:
+    adj.sort()
+#print(adjList)
+
 result = DFS(V, N) #dfs 방문 순서
 print(result)
