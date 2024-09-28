d0075540d7ac6a2a791b795241e2bcbb5eb4d6b4
JUMINAHN <jumin9774@naver.com>
2024-09-28 22:30:50 +0900

    ig

--- Django/Pre-exam study/3029. todo 데이터 조회,생성,수정_orm/accounts/models.py
+++ Django/Pre-exam study/3029. todo 데이터 조회,생성,수정_orm/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    work = models.CharField(max_length=100)
+    content = models.TextField()
+    is_completed = models.BooleanField()
