0d3596de5a77f9ce5c204c6d7b03b5546796ce51
JUMINAHN <jumin9774@naver.com>
2024-11-11 11:35:34 +0900

    update

--- Algorithm/Javascript/문자열 붙여서 출력하기.js
+++ Algorithm/Javascript/문자열 붙여서 출력하기.js
@@ -1,15 +1,15 @@
-const readline = require('readline');
+const readline = require("readline")
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+  input: process.stdin,
+  output: process.stdout,
+})
 
-let input = [];
+let input = []
 
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    str1 = input[0];
-    str2 = input[1];
-    console.log(str1 + str2)
-});
+rl.on("line", function (line) {
+  input = line.split(" ")
+}).on("close", function () {
+  str1 = input[0]
+  str2 = input[1]
+  console.log(str1 + str2)
+})
