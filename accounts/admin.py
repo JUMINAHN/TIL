5839b2b9cd683f371bd28d444dedfe06ee571756
JUMINAHN <jumin9774@naver.com>
2024-12-11 20:59:39 +0900

    django 불필요 파일 삭제

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin #auth와 관련됨
-from .models import User
-
-
-# Register your models here.
-# admin사이트에 등록을 해야함
-# 무엇을 ? User와 그 user를 재정의한 관리자를 
-admin.site.register(User, UserAdmin)
