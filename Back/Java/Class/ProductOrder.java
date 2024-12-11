a9ef3c120aa3aff266ad970bc39bf9fe5ca6cb98
JUMINAHN <jumin9774@naver.com>
2024-12-11 21:23:48 +0900

    file 오류

--- Back/Java/Class/ProductOrder.java
+++
@@ -1,9 +0,0 @@
-package class1;
-
-public class ProductOrder {
-
-    String productName;
-    int price;
-    int quantity;
-
-}
--- Back/Java/Class/Student.java
+++
@@ -1,7 +0,0 @@
-package class1;
-
-public class Student {
-    String name;
-    int age;
-    int grade;
-}
