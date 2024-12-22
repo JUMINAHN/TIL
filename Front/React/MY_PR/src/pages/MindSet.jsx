bb8dd07a431410ac804a0d517c2329f06170a041
JUMINAHN <jumin9774@naver.com>
2024-12-22 21:34:21 +0900

    typing component

--- Front/React/MY_PR/src/pages/MindSet.jsx
+++ Front/React/MY_PR/src/pages/MindSet.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react'
 import './MindSet.css'
 
 const MindSet = () => {
+  // typing
+  const [nowTyping, setNowTyping] = useState('') //일단 빈 값
+  const fullText = 'SSAFY에서 주어진 6개월 간 매일 매일 `성장`하는 것이 목표입니다.'
+  //한개씩 쳐질 것 => 그게 setNowTyping안에 들어가게 되는 것
+  //초마다?
+  useEffect(()=>{
+    // clearinterval적용
+    let nowidx = 0
+    const interval = setInterval(()=>{
+      if (nowidx === fullText.length) {
+        clearInterval(interval)
+        return //fullText를 만족시키면 useEffect 종료~
+      }
+      // 한개씩 증가시키기 => text 값을
+      nowidx++ 
+      setNowTyping(fullText.slice(0, nowidx))
+    },100) //초 한번씩?
+    return () => clearInterval(interval) //interval종료
+  },[])
+  // console.log(nowTyping, 'check')
+
   return (
     <div className="MindSet">
+      {/* <div>
+        <p>{nowTyping}</p>
+      </div> */}
       <section className='title_section'>
-        <h1>SSAFY에서 주어진 6개월 간 매일 매일 `성장`하는 것이 목표입니다.</h1>
+        <h1>{nowTyping}</h1>
+        {/* <h1>SSAFY에서 주어진 6개월 간 매일 매일 `성장`하는 것이 목표입니다.</h1> */}
       </section>
       <section className='content_section'>
         <p>따라서 성장에 진심인 서로 부족한 점을 보완할 수 있는 분들과 함께 하고 싶습니다.</p>
