6c1b0d3d732079ef3cceabdcbf6dafa1c59a5d8e
JUMINAHN <jumin9774@naver.com>
2025-01-03 14:25:01 +0900

    update socket : 2 => 내용 이해가 전혀 안된 상태, 추가 학습 필요

--- Front/Socket/server.js
+++ Front/Socket/server.js
@@ -14,15 +14,36 @@ const io = new Server(server, {
     credentials: true,
   },
 })
-io.on("connection", (socket) => {
+//매개변수 client / socket 모두 가능
+io.on("connection", (client) => {
   //클라이언트가 연결되었을 때
-  console.log("사용자가 입장하였습니다. 박수로 환영해주세요!") //이거 지금 안되잖아 => connect 안된거 아님?
-  // console.log(client) //이건 공식 문서대로 진행되었는데?
-  socket.on("disconnect", (reason) => {
-    console.log(reason)
-    console.log("사용자가 퇴장하였습니다.")
+  // console.log(client) //name은 주소. rooms : map으로 되어있고, id가 있고. nameSpace있고,clients 이쏙,
+  //query에 원하는 데이터를 넣을 수 있음
+  const enterUserName = client.handshake.query.nickName //이 부분
+  console.log(`${enterUserName}가 입장하였습니다. 박수로 환영해주세요!`)
+
+  // 모든 사람에게 전달 => Q. 해당 내용?
+  // client.broadcast.emit("new message", { username: "관리자", message: `${enterUserName}님이 방에 들어왔습니다.` })
+  io.emit("new message", { username: "관리자", message: `${enterUserName}님이 방에 들어왔습니다.` });
+
+  client.on("new message", (msg) => {
+    console.log(`${enterUserName} : `, msg)
+    io.emit("new message", { username: msg.username, message: msg.message })
+  })
+  //else
+  client.on("disconnect", (reason) => {
+    console.log(`${enterUserName}가 퇴장하였습니다.`)
+    //client에 접근이 안됨 => Q. client에 접근이 안됨 => disconnect
+    // 모든 사람
+    io.emit("new message", { username: "관리자", message: `${enterUserName}님이 방에서 나갔습니다.` });    // client.broadcast.emit("new message", { username: "관리자", message: `${enterUserName}님이 방에서 나갔습니다.` })
   })
 })
+// 여기가 맞는가? -> 재연결 동작안함s
+// io.on("reconnect_attempt", (attempt) => {
+//   console.log("재연결을 시도했습니다.")
+//   console.log(attempt)
+// })
+
 server.listen(3000, () => {
   //server가 돌아간다.
   console.log("CORS-enabled web server listening on port 3000")
--- Front/Socket/src/App.jsx
+++ Front/Socket/src/App.jsx
@@ -1,38 +1,80 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
 import { io } from "socket.io-client"
+//이제 사용자의 username을 받아와야 함
+//사용자의 name을 식별할 수 있어야 함 => 이건 자체 local에서만 사용될게 아니기 떄문에 서버에서
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [socket, setSocket] = useState(null) //socket의 연결 유무 => 밑 connect
-  //사용자가 연결을 성공했을 떄
-  const successConnect = () => {
-    // 왜 5173? 3000포트..?
+  const [nickName, setNickName] = useState("")
+  const [socket, setSocket] = useState(null) //연결 링크 및 사용자 정보
+  const [isConnect, setIsConnect] = useState(false) // 연결 유무
+  const [inputMsg, setInputMsg] = useState("")
+  const [message, setMessage] = useState([])
+
+  // 받는메세지
+  const receiveMsg = useCallback((msg) => {
+    console.log(msg)
+    setMessage((prev) => [...prev, msg])
+  }, []) //메세지가 올때마다
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("new message", receiveMsg)
+      return () => {
+        socket.off("new message", receiveMsg)
+      }
+    }
+  }, [socket, receiveMsg]) //socketEvent Listen
+
+  useEffect(() => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      left: 0,
+      behavior: "smooth",
+    })
+  }, [message]) //message가 변경되면 scroll 변경
+
+  const connectSocket = useCallback(() => {
+    if (socket) return // 연결된 경우 재연결 방지
     const _socket = io("http://localhost:3000", {
-      //서버의 주소
-      //여기서는 사용자 서버 자체
+      reconnectionDelayMax: 1, //reconnectDelayMax시간
       autoConnect: false,
-    }) //socket 자체를 사용자에게 불러오는 것이지 => 그니까 async로 매번 불러왔던 것 처럼
-    //내장으로 지금 진행가능하게 하는 것
+      query: {
+        nickName: nickName,
+      },
+    })
     _socket.connect()
-    // console.log(_socket, "_socket")
-    // console.log(_socket.connect(), "_socket")
-    console.log("사용자의 연결이 성공했습니다.") //연결의 성공 유무
-    setSocket(_socket)
-    //reconnect
-    // _socket.on("disconnect", () => {
-    //   socket.connect()
-    //   setSocket(_socket) //socket의 연결 상태
-    // })
+    console.log("사용자의 연결이 성공했습니다.")
+    setSocket(_socket) //socket정보
+    setIsConnect(true)
+  }, [nickName, socket]) //nickname이나 socket 변경시
+
+  const disconnectSocket = useCallback(() => {
+    socket.disconnect()
+    setIsConnect(false)
+    console.log("사용자의 연결이 해제되었습니다.")
+  }, [socket]) //nickname과 무관
+
+  const sendMsg = () => {
+    console.log(`유저의 메세지 입니다 : ${inputMsg}`)
+    if (socket && inputMsg.trim() !== "") {
+      socket.emit("new message", { username: nickName, message: inputMsg })
+      setInputMsg("") //다시 inputMsg초기화
+    }
   }
 
-  const failConnect = () => {
-    //연결 유무에 따라 disconnect
-    socket.disconnect() //단순 disconnect
-    console.log("사용자의 연결이 해제되었습니다.")
-    //socket의 값이 있따면? => disconnect이긴 하겠지만
+  const messageList = message.map((msg, index) => (
+    <li key={index}>
+      {msg.username} : {msg.message}
+    </li>
+  ))
+
+  const onKeyDown = (e) => {
+    if (e.code === "Enter") {
+      sendMsg()
+    }
   }
 
   return (
@@ -45,16 +87,30 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <button onClick={() => successConnect()}>입장</button>
-        <button onClick={() => failConnect()}>퇴장</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {/* 로그인 관련 데이터 입력 */}
+        <div className="card">
+          <h4>당신의 닉네임 : {nickName}</h4>
+
+          <input type="text" name="" id="" placeholder="닉네임을 입력해주세요" value={nickName} onChange={(e) => setNickName(e.target.value)} />
+          <button disabled={isConnect} onClick={() => connectSocket()}>
+            입장
+          </button>
+          <button disabled={!isConnect} onClick={() => disconnectSocket()}>
+            퇴장
+          </button>
+        </div>
+        <h5>{isConnect ? `${nickName}님 환영합니다!` : "방에 입장해주세요!"}</h5>
+        <div className="card">
+          <input type="text" placeholder="message를 입력해주세요" value={inputMsg} onChange={(e) => setInputMsg(e.target.value)} onKeyDown={onKeyDown} />
+          <button onClick={sendMsg}>전송</button>
+        </div>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+
+      <div>
+        <ul>{messageList}</ul>
+      </div>
     </>
   )
 }
