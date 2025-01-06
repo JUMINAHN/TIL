c66dcf75b577935efd3a36ffc514cbe61c8d794b
JUMINAHN <jumin9774@naver.com>
2025-01-06 11:25:50 +0900

    updae files

---
+++ Algorithm/Javascript/a와 b 출력하기.js
@@ -0,0 +1,15 @@
+const readline = require("readline")
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+let input = []
+
+rl.on("line", function (line) {
+  input = line.split(" ") //split
+}).on("close", function () {
+  // console.log(Number(input[0]) + Number(input[1]));
+  console.log(`a = ${input[0]}`)
+  console.log(`b = ${input[1]}`)
+})
